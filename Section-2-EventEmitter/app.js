a7473f87f45d8232526c31b1b0735d270bd39786
CodingRuo <r.schober@outlook.com>
2024-03-27 18:25:13 +0100

    Section 2, Events Done

--- Section-2-EventEmitter/app.js
+++ Section-2-EventEmitter/app.js
@@ -1,11 +1,32 @@
-const EventEmitter = require('events');
+const EventEmitter = require('./events');
 
 class Emitter extends EventEmitter {}
 
 const myE = new Emitter();
 
 myE.on("foo", () => {
-    console.log("Event ocurred");
+    console.log("An event occurred 1");
 })
 
-myE.emit("foo");
+myE.on("foo", () => {
+    console.log("An event occurred 2");
+})
+
+myE.on("foo", (x) => {
+    console.log("An event occurred with parameter");
+    console.log(x);
+})
+
+myE.once("bar", () => {
+    console.log("An event occurred only once");
+})
+
+// myE.emit("foo");
+// myE.emit("foo", "some tex");
+
+myE.emit("bar");
+myE.emit("bar");
+myE.emit("bar");
+myE.emit("bar");
+myE.emit("bar");
+myE.emit("bar");
